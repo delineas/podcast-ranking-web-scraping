cf2d37816302f4fafd56e758f7ec57a918b039ad
Daniel Primo <delineas@gmail.com>
2018-10-30 19:51:32 +0100

    🚧  Add body_class parameter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 # Primero debemos definir la función para luego llamarla
-def extract_toc(filepath):
+def extract_toc(filepath, body_class='#mw-content-text h3'):
     """
         Extrae el TOC de un fichero HTML almacenado en local proviniente de la wikipedia
         Devuelve una lista de items diccionario con title y anchor
@@ -17,13 +17,12 @@ def extract_toc(filepath):
     # TOC es una lista
     toc = []
     # Los titulos del contenido estan en este selector
-    # TODO: Debería ser un selector configurable en la función
-    titles = soup.select('#mw-content-text h3')
+    titles = soup.select(body_class)
     for i in range(0, len(titles)):
         # Leemos los datos hijos de titles (el HTML contiene el ID del anchor)
         for child in titles[i].children:
             # Si el hijo tiene un id entonces añadimos a la lista toc 
-            # TODO: El replace de [edit] debería ser un parámetro de funcion o una tupla de cadenas a eliminar
+            # TODO: El replace de [edit] debería ser un parser posterior
             toc.append({'title': titles[i].getText().replace('[editar]',''), 'anchor': child.get('id')}) if child.get('id') is not None else None
 
     return toc
