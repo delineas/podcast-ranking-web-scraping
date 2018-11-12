42da5a00ad47391ddc4ee728aece49e6434b7e33
Daniel Primo <delineas@gmail.com>
2018-11-12 08:54:15 +0100

    🚧  First version scrapping data

--- app.py
+++ app.py
@@ -14,3 +14,25 @@ def read_file(filepath):
 
     return soup
 
+def get_podcasts(resource, podcasts):
+    """
+        Extrae el listado de podcasts 
+        Devuelve una lista con el diccionario de elementos encontrados
+    """
+    names = []
+    descs = []
+    urls = []
+    for programs in resource.findAll("div", {"class": ["modulo-type-programa"]}):
+        for name in programs.select('meta[itemprop="name"]'):
+            names.append(name.attrs['content'])
+        for desc in programs.select('meta[itemprop="description"]'):
+            descs.append(desc.attrs['content'])
+        for url in programs.select('meta[itemprop="url"]'):
+            urls.append(url.attrs['content'])
+
+    podcasts = [names, descs, urls]
+    return podcasts
+
+podcasts = []
+podcasts = get_podcasts(read_file("sandbox/content/podcast_internet_tecnologia_1.html"), podcasts)
+pprint(podcasts)
