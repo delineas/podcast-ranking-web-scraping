90f1737f2819dee44110bf29072a04b5f2c769fe
webreactiva <delineas+webreactiva@gmail.com>
2018-11-18 22:18:27 +0100

    🐎  Remove deprecated code

--- app.py
+++ app.py
@@ -1,53 +1,8 @@
-from bs4 import BeautifulSoup
 from pprint import pprint
-import requests
 import json
 from ivoox_scraper_client import IvooxScraperClient
+from ranker import Ranker
 
-def read_file(filepath):
-    """
-        Extrae el contenido de ficheros locales y comprueba si existen
-    """
-    try:
-        file = open(filepath)
-    except FileNotFoundError:
-        print("El fichero no existe") 
-    
-    soup = BeautifulSoup(file.read(), "lxml")
-
-    return soup
-
-def get_podcasts(resource, podcasts):
-    """
-        Extrae el listado de podcasts 
-        Devuelve una lista con el diccionario de elementos encontrados
-    """
-    for programs in resource.findAll("div", {"class": ["modulo-type-programa"]}):
-        try:
-            podcasts.append(
-                {
-                    'title': ["%s" % name.attrs['content'] for name in programs.select('meta[itemprop="name"]')][0],
-                    'description': ["%s" % desc.attrs['content'] for desc in programs.select('meta[itemprop="description"]')][0],
-                    'url': ["%s" % url.attrs['content'] for url in programs.select('meta[itemprop="url"]')][0],
-                    'episodes': ["%s" % micro.getText().strip() for micro in programs.select('.microphone')][0]
-                }
-            )
-        except IndexError:
-            print("No se puede captuar el contenido") 
-
-    return podcasts
-
-# podcasts = []
-# # get_podcasts(read_file("sandbox/content/podcast_internet_tecnologia_1.html"), podcasts)
-# # get_podcasts(read_file("sandbox/content/podcast_internet_tecnologia_2.html"), podcasts)
-# get_podcasts(get_url_content("https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html"), podcasts)
-# get_podcasts(get_url_content("https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_2.html"), podcasts)
-# pprint(podcasts)
-
-podcasts = []
-ivooxScrapperClient = IvooxScraperClient('internet-tecnologia',445)
-get_podcasts(ivooxScrapperClient.request(1), podcasts)
-get_podcasts(ivooxScrapperClient.request(2), podcasts)
-get_podcasts(ivooxScrapperClient.request(3), podcasts)
-get_podcasts(ivooxScrapperClient.request(4), podcasts)
+ranker = Ranker('internet-tecnologia',445,3)
+podcasts = ranker.build()
 print(json.dumps(podcasts))
---
+++ ranker.py
@@ -0,0 +1,42 @@
+from ivoox_scraper_client import IvooxScraperClient
+
+class Ranker:
+    """
+        Clase para obtener el listado de podcasts
+    """
+    def __init__(self,category_name, category_id,  max_page):
+        self.podcasts = []
+        self.max_page = max_page
+        self.category_name = category_name
+        self.category_id = category_id
+    
+    def build(self):
+        """
+            Construye el listado
+        """
+        ivooxScrapperClient = IvooxScraperClient(self.category_name, self.category_id)
+        for page in range(1, self.max_page):
+            self.__parse_resource(ivooxScrapperClient.request(page))
+        
+        return self.podcasts
+
+    def get_podcasts(self):
+        return self.podcasts
+
+    def __parse_resource(self, resource):
+        """
+            Extrae el listado de podcasts 
+            Devuelve una lista con el diccionario de elementos encontrados
+        """
+        for programs in resource.findAll("div", {"class": ["modulo-type-programa"]}):
+            try:
+                self.podcasts.append(
+                    {
+                        'title': ["%s" % name.attrs['content'] for name in programs.select('meta[itemprop="name"]')][0],
+                        'description': ["%s" % desc.attrs['content'] for desc in programs.select('meta[itemprop="description"]')][0],
+                        'url': ["%s" % url.attrs['content'] for url in programs.select('meta[itemprop="url"]')][0],
+                        'episodes': ["%s" % micro.getText().strip() for micro in programs.select('.microphone')][0]
+                    }
+                )
+            except IndexError:
+                print("No se puede captuar el contenido") 
