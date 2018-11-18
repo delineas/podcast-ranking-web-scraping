7f3e4c1d070d2f796962e54a2579b7201e921304
webreactiva <delineas+webreactiva@gmail.com>
2018-11-18 22:19:56 +0100

    📚  Add comment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from ivoox_scraper_client import IvooxScraperClient
 from ranker import Ranker
 
+# Get https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html (3 pages of results)
 ranker = Ranker('internet-tecnologia',445,3)
 podcasts = ranker.build()
 print(json.dumps(podcasts))
