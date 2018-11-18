a4517ad35fbf88cee477f7a99c70630c322e3ac0
Daniel Primo <delineas@gmail.com>
2018-11-19 00:42:16 +0100

    🐎  Better storage implementation

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from pprint import pprint
 import json
 from ivoox_scraper_client import IvooxScraperClient
 from ranker import Ranker
+from storage import Storage
 
 # Get https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html (3 pages of results)
-ranker = Ranker('internet-tecnologia',445,3)
-podcasts = ranker.build()
-print(json.dumps(podcasts))
+podcasts = Ranker('internet-tecnologia',445,3).build()
+Storage.save('storage/ranking.json', podcasts)
