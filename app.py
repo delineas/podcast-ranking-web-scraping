c8563997d7fe5a24cab092a7b91fdce8d5ef3578
Daniel Primo <delineas@gmail.com>
2018-11-25 22:03:49 +0100

    ♻️ Remove dependencies. Use datetime in storage filename.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pprint import pprint
-import json
-from ivoox_scraper_client import IvooxScraperClient
 from ranker import Ranker
 from storage import Storage
+import datetime
+
+now = datetime.datetime.now()
 
 # Get https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html (3 pages of results)
 podcasts = Ranker('internet-tecnologia',445,3).build()
-Storage.save('storage/ranking.json', podcasts)
+Storage.save('storage/ranking_{0}-{1}-{2}.json'.format(now.year,now.strftime('%m'),now.strftime('%d')), podcasts)
