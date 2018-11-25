9e57aeadd7008b8e0942c303df49a5b446cab36f
Daniel Primo <delineas@gmail.com>
2018-11-25 22:30:29 +0100

    🚧 Get first 5 pages

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ import datetime
 now = datetime.datetime.now()
 
 # Get https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html (3 pages of results)
-podcasts = Ranker('internet-tecnologia',445,3).build()
+podcasts = Ranker('internet-tecnologia',445,5).build()
 Storage.save('storage/ranking_{0}-{1}-{2}.json'.format(now.year,now.strftime('%m'),now.strftime('%d')), podcasts)
