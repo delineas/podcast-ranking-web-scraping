5e5dab6d28189384bcef3f6c6e910b843d8cf60a
Daniel Primo <delineas@gmail.com>
2018-11-25 22:06:02 +0100

    ♻️ Capture get file from storage and date from filename

--- storage.py
+++ storage.py
@@ -1,7 +1,12 @@
 import json
+import glob
+import os
+import re
+import datetime
 
 class Storage:
 
+ 
     def save(filename, data):
         with open(filename, 'w') as file:  
             json.dump(data, file, ensure_ascii=False)
@@ -10,3 +15,18 @@ class Storage:
         with open(filename) as file:  
             data = json.load(file)
         return data
+
+    def last_file():
+        files = glob.glob('storage/ranking*') 
+        latest = max(files, key=os.path.getctime)
+        return latest
+
+    def load_last_file():
+        file = Storage.last_file()
+        return Storage.load(file)
+
+    def get_date(ranking_name):
+        regex = r"(\d{4})-(\d{2})-(\d{2})"
+        matches = re.search(regex, ranking_name)
+        date = datetime.datetime.strptime(matches.group(), '%Y-%m-%d')
+        return date
