3d85d91122875c800d62c0f9e2139b7f26327e3d
Daniel Primo <delineas@gmail.com>
2018-12-17 13:39:00 +0100

    🚧  Add check file exists and force generation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from storage import Storage
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    if not Storage.check_files_exists():
+        os.system("python3 app.py")
     ranking = Storage.load_last_file()
     date = Storage.get_date(Storage.last_file())
     return render_template("home.html", ranking=ranking, date=date)
